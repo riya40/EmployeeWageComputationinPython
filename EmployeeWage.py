2299f463b8246d52a59b57498bf5b0b10ad4a8e8
riya40 <priyanunna20@gmail.com>
2022-02-18 23:39:10 +0530

    Added:Checking Employee Present or Not

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -1,1 +1,13 @@
+import random
+"""
+Performing the Employee wage computaion
+checking Employee present or not
+"""
 print("Welcome to Employee Wage Computation")
+
+attendence = random.randint(0, 1)
+
+if attendence == 1:
+    print("employee is present:", attendence)
+else:
+    print("employee is absent:", attendence)
