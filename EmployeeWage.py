56286e2f24723e5112e4c265dbf02e3544f8bb92
riya40 <priyanunna20@gmail.com>
2022-02-25 23:27:39 +0530

    Added:Parttimer wage calculation

--- EmployeeWage.py
+++ EmployeeWage.py
@@ -1,18 +1,26 @@
 import random
+
 """
 Performing the Employee wage computation
 checking Employee present or not
 Adding the wage_per_day and full_day_hour
+Adding the parttimer and hour
 """
 print("Welcome to Employee Wage Computation")
 
 WAGE_PER_HR = 20
 FULL_DAY_HR = 8
+PART_TIME_HR = 6  # adding parttime wage
 
+attendance = random.randint(0, 2)
 
-attendence = random.randint(0, 2)
-
-print(f'Attendance {attendence}')
-
-wage_per_day = WAGE_PER_HR * FULL_DAY_HR
-print('Wage per day {}'.format(wage_per_day))
+if attendance == 0:
+    print("absent")
+elif attendance == 1:  # adding partime
+    hours = PART_TIME_HR
+    wage = PART_TIME_HR * WAGE_PER_HR
+    print("Today Present but par time wage is", wage, hours)
+else:
+    hours = FULL_DAY_HR
+    wage = FULL_DAY_HR * WAGE_PER_HR
+    print("today present and for full day", wage, hours)
